51c1f8ee8c6f3b0ce54d9ee4f44bf1913c3e4b65
Aleksander M <a.mozhaev94@gmail.com>
2021-05-15 21:00:58 +0600

    add new game brain-even

--- src/games/brain-even.php
+++ src/games/brain-even.php
@@ -1,2 +1,28 @@
 <?php
 
+namespace Brain\Game\Games\Brain\Even;
+
+use function cli\line;
+use function cli\prompt;
+use function Brain\Games\Helper\Function\welcome;
+use function Brain\Games\Games\Engine\gameEngine;
+use function Brain\Games\Cli\getUserName;
+
+function gameBrainEven()
+{
+    $gameLogic = function () {
+        $answers = [];
+        $number = rand(1, 100);
+        $correctAnswer = $number % 2 === 0 ? 'yes' : 'no';
+        line("Question: {$number}");
+        $userAnswer = prompt("Your answer");
+        $answers[] = $correctAnswer;
+        $answers[] = $userAnswer;
+        $answers[] = $correctAnswer === $userAnswer;
+        return $answers;
+    };
+    welcome();
+    $name = getUserName();
+    print_r("Answer \"yes\" if the number is even, otherwise answer \"no\".\n");
+    gameEngine($name, $gameLogic);
+}
--- src/games/engine.php
+++ src/games/engine.php
@@ -1,9 +1,23 @@
 <?php
 
-namespace Brain\Games\Games;
+namespace Brain\Games\Games\Engine;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-use function Brain\Games\Cli\getUserName;
-
-getUserName();
+function gameEngine($name, $game)
+{
+    $counterCorrectAnswer = 0;
+    while ($counterCorrectAnswer < 3) {
+        $answers = $game();
+        $correctAnswer = $answers[0];
+        $userAnswer =  $answers[1];
+        $resultGame = $answers[2];
+        if ($resultGame === true) {
+            $counterCorrectAnswer += 1;
+            print_r("Correct\n");
+        } else {
+            print_r("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.\n");
+            print_r("Let's try again, {$name}!\n");
+            return null;
+        }
+    }
+    print_r("Congratulations, {$name}!\n");
+}
