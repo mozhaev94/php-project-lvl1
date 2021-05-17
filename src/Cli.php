b2c2169afec8d6a6c00ab4183d1b461d0904c3b4
Aleksander M <a.mozhaev94@gmail.com>
2021-05-17 15:36:17 +0600

    fix errors linter

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function getUserName()
+function getUserName(): string
 {
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
